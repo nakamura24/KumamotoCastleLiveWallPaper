5890e6f42a6076bf8b635a30d5f6bac2cc406129
M.Nakamura <zmf42190@gmail.com>
2013-01-03 22:05:05 +0900

    1.1.8

--- src/jp/kumamotocastlelivewallpaper/LiveWallPaperPreference.java
+++ src/jp/kumamotocastlelivewallpaper/LiveWallPaperPreference.java
@@ -1,4 +1,14 @@
-﻿package jp.kumamotocastlelivewallpaper;
+﻿/*
+ * Copyright (C) 2012 M.Nakamura
+ *
+ * This software is licensed under a Creative Commons
+ * Attribution-NonCommercial-ShareAlike 2.1 Japan License.
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 		http://creativecommons.org/licenses/by-nc-sa/2.1/jp/legalcode
+ */
+
+package jp.kumamotocastlelivewallpaper;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
